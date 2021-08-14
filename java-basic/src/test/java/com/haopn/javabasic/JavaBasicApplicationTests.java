e6460dc508ced74265f066a307ead3e3e8ae8502
haopn <haopn@vng.com.vn>
2021-08-14 15:16:08 +0700

    add general project for java

---
+++ java-basic/src/test/java/com/haopn/javabasic/JavaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haopn.javabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
